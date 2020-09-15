20b276408061b7e4c6e975efc5da63c6077868a1
EvelynGYX <evelynguo410@gmail.com>
2020-09-15 15:28:13 +1000

    add file system feature

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,5 +1,24 @@
+import * as FileSystem from 'expo-file-system';
+
 export const ADD_PLACE = 'ADD_PLACE';
 
-export const addPlace = (title) => {
-    return { type: ADD_PLACE, placeData: { title } };
+export const addPlace = (title, image) => {
+    return async dispatch => {
+        const fileName = image.split('/').pop(); // get last item
+        const newPath = FileSystem.documentDirectory + fileName;
+
+        try {
+            // move file
+            await FileSystem.moveAsync({
+                from: image,
+                to: newPath
+            });
+        } catch (err) {
+            console.log(err);
+            throw err;
+        }
+
+        // store permanent file directory in memory but not store data itself in a permanent storage
+        dispatch({ type: ADD_PLACE, placeData: { title, image: newPath } });
+    };
 };
